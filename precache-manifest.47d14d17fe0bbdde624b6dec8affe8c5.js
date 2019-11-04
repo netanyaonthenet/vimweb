a7ded24b09a8791e3f3d2519082f1a18b86ec8f4
Netanya Trowers <netanyatrowers@nbp-220-78.nbp.ruw.rutgers.edu>
2019-11-04 13:31:39 -0500

    Updates

---
+++ precache-manifest.47d14d17fe0bbdde624b6dec8affe8c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad3606cb089ece5846acfa03338b0b6",
+    "url": "/vimweb/index.html"
+  },
+  {
+    "revision": "947bdd9f3e1a56b6327b",
+    "url": "/vimweb/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "0c3521cd274b3e9ca9bb",
+    "url": "/vimweb/static/css/main.66efeacb.chunk.css"
+  },
+  {
+    "revision": "947bdd9f3e1a56b6327b",
+    "url": "/vimweb/static/js/2.ea2d21e1.chunk.js"
+  },
+  {
+    "revision": "0c3521cd274b3e9ca9bb",
+    "url": "/vimweb/static/js/main.43c071d3.chunk.js"
+  },
+  {
+    "revision": "4aa05442d17bd11dfd98",
+    "url": "/vimweb/static/js/runtime-main.4646f3d1.js"
+  },
+  {
+    "revision": "cc4cd2a84c8baf96727a58d04875e824",
+    "url": "/vimweb/static/media/poster1.cc4cd2a8.jpg"
+  },
+  {
+    "revision": "ab3ec610acf09808f66a937ec2eed77c",
+    "url": "/vimweb/static/media/poster2.ab3ec610.jpg"
+  },
+  {
+    "revision": "3b92c41720e9e37b58c8c5296964ff83",
+    "url": "/vimweb/static/media/poster3.3b92c417.jpg"
+  }
+]);
